29a83bbe869e6f65c8f354b571507ded3862afe8
ljhyeon <gee9328@gmail.com>
2025-06-12 23:54:47 +0900

    fix: dictionary 형식 변경

--- frontend/my-app/dev-dist/sw.js
+++ frontend/my-app/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ht79ofbsn8"
+    "revision": "0.n1saogiqf5g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
