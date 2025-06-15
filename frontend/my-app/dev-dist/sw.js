55f24c3f15b59ab3a3c891f60d907fbd30bc460d
Lee <sos000523@naver.com>
2025-06-15 17:32:17 +0900

    설치 파일

--- frontend/my-app/dev-dist/sw.js
+++ frontend/my-app/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.36ui7vaghd"
+    "revision": "0.ilogiqserr8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
